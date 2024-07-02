190eec2e67ba5dc06e05d675fa985df5bee72c02
ruhollah-naseri <ruhollah_82@outlook.com>
2024-07-02 17:03:08 +0330

    removed inptut

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./AddUser.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
-import Input from "../UI/Input";
 
 const AddUser = (props) => {
   const AddUserHandler = (event) => {
@@ -13,9 +12,9 @@ const AddUser = (props) => {
     <Card>
       <form onSubmit={AddUserHandler} className="userform">
         <lable htmlFor="username">Username</lable>
-        <Input id="username" type="text" className="userInput" />
+        <input id="username" type="text" className="userInput" />
         <lable htmlFor="age">age (years)</lable>
-        <Input id="age" type="number" className="userInput" />
+        <input id="age" type="number" className="userInput" />
         <Button type="submit">Add user</Button>
       </form>
     </Card>
